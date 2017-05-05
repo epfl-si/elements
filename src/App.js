8222fddb9b00653581eaa62805b63ad78d0ae2ac
Yann Gouffon <github@yago-art.ch>
2017-05-05 15:30:36 +0200

    ✨ add base twig rendering

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
+/* global twig */
+
 import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      content: 'yo',
+    }
+  }
+
+  componentWillMount() {
+    this.content = twig({
+      id: 'button',
+      href: '/components/button.twig',
+      async: true,
+      load: (template) => {
+        const content = template.render({ label: 'my button' });
+        this.setState({ content });
+      }
+    });
+  }
+
   render() {
     return (
       <div className="App">
         <h1>hello Toolbox</h1>
+        <div dangerouslySetInnerHTML={{__html: this.state.content}}></div>
       </div>
     );
   }
