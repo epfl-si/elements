dded9f01a0893fade0cbf1277094889f35e5ab03
Yann Gouffon <github@yago-art.ch>
2017-07-28 08:44:47 +0200

    📦 updates builds

--- build/js/app.bundle.js
+++
@@ -1,1 +0,0 @@
-webpackJsonp([0],[function(e,n,t){"use strict";Object.defineProperty(n,"__esModule",{value:!0});var o=function(){var e=new XMLHttpRequest;e.open("GET","<%= iconsPath %>",!0),e.send(),e.onload=function(n){var t=document.createElement("div");t.innerHTML=e.responseText,document.body.insertBefore(t,document.body.childNodes[0])}};n.default=o},function(e,n,t){"use strict";t(0)}],[1]);
