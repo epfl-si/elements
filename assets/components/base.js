35776570c992b8910e9ac24ac84daa89efa217f3
Yann Gouffon <github@yago-art.ch>
2018-03-09 13:56:05 +0100

    ✨ add base datepicker #6

--- assets/components/base.js
+++ assets/components/base.js
@@ -1,12 +1,14 @@
 /* globals jQuery */
 
 import upload from './atoms/upload/upload';
+import datebpicker from './molecules/datepicker/datepicker';
 import svgIcons from '../icons/svg-icons';
 
 svgIcons(); // Must run as soon as possible
 
 const init = () => {
   upload();
+  datebpicker();
 };
 
 (function ($) {
