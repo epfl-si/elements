d9ab66af6f16d8f88efa50ef4245c7c88aebfefd
Yann Gouffon <github@yago-art.ch>
2018-03-09 11:08:42 +0100

    ✨ fix upload field #6

--- assets/components/base.js
+++ assets/components/base.js
@@ -1,12 +1,15 @@
 /* globals jQuery */
 
-import upload from './atoms/upload/upload'
+import upload from './atoms/upload/upload';
 import svgIcons from '../icons/svg-icons';
 
 svgIcons(); // Must run as soon as possible
 
+const init = () => {
+  upload();
+};
+
 (function ($) {
-  $(document).ready(() => {
-    upload();
-  });
+  $(document).ready(() => init());
 })(jQuery);
+document.addEventListener('ToolboxReady', () => init());
