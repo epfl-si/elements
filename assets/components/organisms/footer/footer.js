8299d8856ed07ae1d2d6a3cf2464fecbe2a31d2a
knuch <knuchelf@gmail.com>
2018-03-20 14:42:59 +0100

    disable tablesaw, refactor links name, allow toggle on mobile

--- assets/components/organisms/footer/footer.js
+++ assets/components/organisms/footer/footer.js
@@ -4,7 +4,12 @@ export default () => {
   $('.footer-title').each(
     function () {
       $(this).on('click', function () {
-        $(this).parent().toggleClass('active');
+        if ($(this).parent().hasClass('active')) {
+          $(this).parent().removeClass('active');
+        } else {
+          $('.footer-collapse').each(function () { $(this).removeClass('active'); });
+          $(this).parent().addClass('active');
+        }
       });
     },
   );
