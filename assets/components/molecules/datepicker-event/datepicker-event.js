c56ab0d34511af8b7009cad0a3a30a40f575d725
knuch <knuchelf@gmail.com>
2018-03-23 17:27:10 +0100

    handle day selection, active date, and test animation

--- assets/components/molecules/datepicker-event/datepicker-event.js
+++ assets/components/molecules/datepicker-event/datepicker-event.js
@@ -5,35 +5,41 @@ export default () => {
     if ($('.datepicker-event').length > 0) {
       $('.datepicker-event').parent().each(function() {
         let weekNum = 1;
+        const container = this;
 
-        const datepicker = this;
         // only setup listeners when on mobile
-        const isMobile = $('table', datepicker).css('display') === 'block';
-        if (!isMobile) return datepicker;
+        const isMobile = $('table', container).css('display') === 'block';
+        if (!isMobile) return container;
 
         // initialise data attibute
-        $('tbody', datepicker).attr('data-week', weekNum);
 
-        $('.picker__nav--next', datepicker).on('click', function(e) {
+        const selected = $('tr:has(.picker__day--selected)', container);
+        if (selected.length > 0) {
+          weekNum = $('tr', container).index(selected);
+        }
+
+        $('tbody', container).attr('data-week', weekNum);
+
+        $('.picker__nav--next', container).on('click', function(e) {
           if (weekNum < 6) {
             // if we are not displaying the last row:
             // disable built-in behaviour and update data attribute
             e.stopPropagation();
             weekNum += 1;
-            $('tbody', datepicker).attr('data-week', weekNum);
+            $('tbody', container).attr('data-week', weekNum);
           } else {
             // let plugin proceed to next month, reset listeners
             handlePrevNext();
           }
         });
 
-        $('.picker__nav--prev', datepicker).on('click', function(e) {
+        $('.picker__nav--prev', container).on('click', function(e) {
           if (weekNum > 1) {
             // if we are not displaying the first row:
             // disable built-in behaviour and update visual to show previous week
             e.stopPropagation();
             weekNum -= 1;
-            $('tbody', datepicker).attr('data-week', weekNum);
+            $('tbody', container).attr('data-week', weekNum);
           } else {
             // let plugin proceed to last month, reset listeners
             handlePrevNext();
