8c4261ca2cd2488ec06c29674c6b3986ce131369
knuch <knuchelf@gmail.com>
2018-05-24 11:41:52 +0200

    refactor after review

--- assets/components/molecules/nav/nav.js
+++ assets/components/molecules/nav/nav.js
@@ -9,14 +9,14 @@ const nav = () => {
     $('.main-container').toggleClass('mobile-menu-open');
   };
 
-  $('.nav .nav-back a').on('click', function (e) {
+  $('.nav-main .nav-back a').on('click', function (e) {
     e.preventDefault();
     $(this).parent().parent().parent().removeClass(activeClass);
     $(this).parent().parent().parent().parent().parent().removeClass(parentClass);
     $(this).parent().parent().parent().parent().parent().addClass(activeClass);
   });
 
-  $('.nav .nav-arrow').on('click', function (e) {
+  $('.nav-main .nav-arrow').on('click', function (e) {
     e.preventDefault();
     $(this).parent().parent().parent().addClass(parentClass);
     $(this).parent().parent().parent().removeClass(activeClass);
@@ -26,33 +26,6 @@ const nav = () => {
   $('.menu-toggle-mobile').on('click', function () {
     toggleMobileMenu();
   });
-
- /*  $('#mobile-nav .toggle').on('click', function (e) {
-    e.preventDefault();
-
-    const link = $('#mobile-nav li.active');
-
-    if (link.length > 0) {
-      //activateMobileParents(link);
-    } else {
-      //toggleMobileMenu();
-    }
-  });
-
-  $('.mobile-overlay').on('click', function () {
-    //closeMobileMenu();
-  });
-
-  $('#mobile-nav .back').on('click', function (e) {
-    e.preventDefault();
-    $(this).parent().parent().attr('style', '');
-
-    if ($(this).parent().parent().parent().parent().parent().hasClass('navigation-container')) {
-      $(this).parent().parent().parent().parent().css('left', '50%');
-    } else {
-      $(this).parent().parent().parent().parent().css('left', '100%');
-    }
-  }); */
 };
 
 export default nav;
