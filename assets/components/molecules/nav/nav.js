a69d3ca42be783bae1c182c89ef932bcc803edf0
knuch <knuchelf@gmail.com>
2018-05-24 17:23:37 +0200

    nav toggle

--- assets/components/molecules/nav/nav.js
+++ assets/components/molecules/nav/nav.js
@@ -5,9 +5,24 @@ const nav = () => {
   const activeClass = 'current_page_parent';
   const parentClass = 'current_page_ancestor';
 
+  const resetMenu = () => {
+    $('.nav-main li').removeClass(activeClass);
+    $('.nav-main li').removeClass(parentClass);
+  };
+
   const toggleMobileMenu = () => {
     $('.main-container').toggleClass('mobile-menu-open');
-    $('.menu-toggle-mobile').toggleClass('menu-toggle-mobile-opened');
+    $('.menu-toggle-mobile').toggleClass('menu-toggle-mobile-open');
+  };
+
+  const openDesktopMenu = (id) => {
+    resetMenu();
+    $('.nav-main a[data-page-id="' + id + '"]').parent().addClass(activeClass);
+    $('.main-container').addClass('desktop-menu-open');
+  };
+
+  const toggleDesktopMenu = () => {
+    $('.main-container').toggleClass('desktop-menu-open');
   };
 
   $('.nav-main .nav-back a').on('click', function (e) {
@@ -26,6 +41,29 @@ const nav = () => {
 
   $('.menu-toggle-mobile').on('click', function () {
     toggleMobileMenu();
+  });
+
+  $('.header-nav-toggle a').on('click', function (e) {
+    e.preventDefault();
+    const id = $(this).data('page-id');
+    if ($('.desktop-menu-open').length > 0) {
+      toggleDesktopMenu();
+      setTimeout(function() {
+        openDesktopMenu(id);
+      }, 300);
+    } else {
+      openDesktopMenu(id);
+    }
+  });
+
+  $('.nav-close').on('click', function (e) {
+    e.preventDefault();
+    toggleDesktopMenu();
+  });
+
+  $('.overlay').on('click', function (e) {
+    e.preventDefault();
+    toggleDesktopMenu();
   });
 };
 
