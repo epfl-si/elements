4455e109b2b68dd5711004988a4ad03fd40f0a8e
Julien Minguely <jminguely@gmail.com>
2018-05-28 09:30:37 +0200

    Corrections: pass the js to something more 2015 friendly

--- assets/components/atoms/tag/tag-input.js
+++ assets/components/atoms/tag/tag-input.js
@@ -4,16 +4,11 @@ export default () => {
   $('.selectize').selectize({
     plugins: ['remove_button'],
     render: {
-      item: function(data, escape) {
-        console.log(data, escape);
-        return '<div class="badge tag tag-primary">' + escape(data.text) + '</div>';
-      }
+      item: (data, escape) => `<div class="tag tag-primary">${escape(data.text)}</div>`,
     },
-    create: function(input) {
-        return {
-            value: input,
-            text: input
-        }
-    }
+    create: input => ({
+      value: input,
+      text: input,
+    }),
   });
 };
