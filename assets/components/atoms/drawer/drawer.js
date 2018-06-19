dd83d72f8a573c4e2b473b8d88c9df511d8ae70e
Yann Gouffon <github@yago-art.ch>
2018-06-19 10:10:05 +0200

    🔨 refactor header-light before merging

--- assets/components/atoms/drawer/drawer.js
+++ assets/components/atoms/drawer/drawer.js
@@ -1,28 +1,23 @@
 /* global $ */
 
 const drawer = () => {
-  $('.drawer-toggle').click((e) => {
-    e.preventDefault();
-    const $drawer = $(e.currentTarget).parent('.drawer');
+  $('.drawer-toggle').click(function () {
+    const $drawer = $(this).parent('.drawer');
     const $drawerLink = $drawer.find('.drawer-link');
-    const $drawerToggle = $drawer.find('.drawer-toggle');
+    const $drawerToggle = $(this);
     const breakpoint = 992;
 
     if ($drawer.hasClass('open')) {
-      $drawer
-        .removeClass('open');
-      $drawerLink
-        .css({ width: $drawerToggle.width() });
+      $drawer.removeClass('open');
+      $drawerLink.css({ width: $drawerToggle.width() });
     } else {
       let linkWidth = '100%';
 
       if ($(window).width() > breakpoint) {
         linkWidth = $drawerLink.find('.text').outerWidth();
       }
-      $drawer
-        .addClass('open');
-      $drawerLink
-        .css({ width: linkWidth });
+      $drawer.addClass('open');
+      $drawerLink.css({ width: linkWidth });
     }
   });
 };
