80477b69b5eb1082ee7e32f92c1e103c6a0a874f
knuch <knuchelf@gmail.com>
2018-09-10 09:43:31 +0200

    improve js readability

--- assets/components/content-types/coursebook/coursebook.js
+++ assets/components/content-types/coursebook/coursebook.js
@@ -3,11 +3,14 @@
 export default () => {
   const id = new Date().getTime().toString(16);
   $('.coursebook-program .tree>li').each((i, collapse) => {
+
     // identify toggle and target
     const toggle = $('.underline a', collapse);
     const target = $('>ul', collapse);
+
     // generate page-unique id
     const itemId = `${id}-${i}`;
+
     // set correct attributes
     target.addClass('collapse collapse-item collapse-item-desktop');
     toggle.addClass('collapse-title collapse-title-desktop collapsed');
