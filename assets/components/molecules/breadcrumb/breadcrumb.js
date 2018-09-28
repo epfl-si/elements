be8f26d2b94bea12737bbc10e15c032d8b0f83ea
jdelasoie <julien.delasoie@epfl.ch>
2018-09-28 14:02:07 +0200

    Fix the javascript error in case of the breadcrumb is not used (#309)
    * Fix the javascript error in case of the breadcrumb is not used

--- assets/components/molecules/breadcrumb/breadcrumb.js
+++ assets/components/molecules/breadcrumb/breadcrumb.js
@@ -2,45 +2,48 @@
 
 export default () => {
   const breadcrumb = $('#breadcrumb-wrapper');
-  const breadcrumbNode = breadcrumb[0];
-  const breadcrumbComponent = breadcrumb.find('.breadcrumb');
 
-  if ($(window).width() > 1199 && breadcrumb.width() < breadcrumbComponent[0].scrollWidth) {
-    let isDown = false;
-    let startX;
-    let scrollLeft;
+  if ($(breadcrumb).length > 0) {  // don't expect to have the breadcrumb on every case
+    const breadcrumbNode = breadcrumb[0];
+    const breadcrumbComponent = breadcrumb.find('.breadcrumb');
 
-    breadcrumb.on('mousedown', (e) => {
-      isDown = true;
-      breadcrumb.addClass('moving');
-      startX = e.pageX - breadcrumbNode.offsetLeft;
-      // eslint-disable-next-line
-      scrollLeft = breadcrumbNode.scrollLeft;
-    });
+    if ($(window).width() > 1199 && breadcrumb.width() < breadcrumbComponent[0].scrollWidth) {
+      let isDown = false;
+      let startX;
+      let scrollLeft;
 
-    breadcrumb.on('mouseleave', () => {
-      isDown = false;
-      breadcrumb.removeClass('moving');
-    });
+      breadcrumb.on('mousedown', (e) => {
+        isDown = true;
+        breadcrumb.addClass('moving');
+        startX = e.pageX - breadcrumbNode.offsetLeft;
+        // eslint-disable-next-line
+        scrollLeft = breadcrumbNode.scrollLeft;
+      });
 
-    breadcrumb.on('mouseup', () => {
-      isDown = false;
-      breadcrumb.removeClass('moving');
-    });
+      breadcrumb.on('mouseleave', () => {
+        isDown = false;
+        breadcrumb.removeClass('moving');
+      });
 
-    breadcrumb.on('mousemove', (e) => {
-      if (!isDown) return; // stop the fn from running
-      e.preventDefault();
-      const x = e.pageX - breadcrumbNode.offsetLeft;
-      const walk = (x - startX) * 3;
-      breadcrumbNode.scrollLeft = scrollLeft - walk;
-    });
+      breadcrumb.on('mouseup', () => {
+        isDown = false;
+        breadcrumb.removeClass('moving');
+      });
 
-    breadcrumb.mousewheel((e, delta) => {
-      e.preventDefault();
-      breadcrumbNode.scrollLeft -= delta * 40;
-    });
+      breadcrumb.on('mousemove', (e) => {
+        if (!isDown) return; // stop the fn from running
+        e.preventDefault();
+        const x = e.pageX - breadcrumbNode.offsetLeft;
+        const walk = (x - startX) * 3;
+        breadcrumbNode.scrollLeft = scrollLeft - walk;
+      });
 
-    breadcrumb.find('*').on('dragstart', () => false);
+      breadcrumb.mousewheel((e, delta) => {
+        e.preventDefault();
+        breadcrumbNode.scrollLeft -= delta * 40;
+      });
+
+      breadcrumb.find('*').on('dragstart', () => false);
+    }
   }
 };
