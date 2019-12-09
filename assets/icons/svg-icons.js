c2f3246075f74e125af00f951805df88eacc3f42
William Belle <william.belle@gmail.com>
2019-12-09 21:01:18 +0100

    add feather icons

--- assets/icons/svg-icons.js
+++ assets/icons/svg-icons.js
@@ -4,9 +4,24 @@ const svgIcons = () => {
   ajax.open('GET', svgPath, true);
   ajax.send();
   ajax.onload = function (e) {
-    var div = document.createElement('div');
-    div.innerHTML = ajax.responseText;
-    document.body.insertBefore(div, document.body.childNodes[0]);
+    if (e.target.status === 200) {
+      const div = document.createElement('div');
+      div.innerHTML = ajax.responseText;
+      document.body.insertBefore(div, document.body.childNodes[0]);
+    }
+  };
+
+  const ajaxFeather = new XMLHttpRequest();
+  const featherSvgPath = window.featherSvgPath || 'icons/feather-sprite.svg';
+  ajaxFeather.open('GET', featherSvgPath, true);
+  ajaxFeather.send();
+  ajaxFeather.onload = function (e) {
+    if (e.target.status === 200) {
+      const div = document.createElement('div');
+      div.innerHTML = ajaxFeather.responseText;
+      div.style = 'display: none;';
+      document.body.insertBefore(div, document.body.childNodes[0]);
+    }
   };
 };
 
