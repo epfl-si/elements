576e472a116d6583cdebeb2e54ebb3c28fb0fe33
Grégory Charmier <gregory.charmier@gmail.com>
2020-04-07 15:57:27 +0200

    Mutiple memento slider by pages

--- assets/components/organisms/card-slider/card-slider.js
+++ assets/components/organisms/card-slider/card-slider.js
@@ -4,43 +4,52 @@ import Flickity from 'flickity';
 import Imagesloaded from 'imagesloaded';
 
 export default () => {
-  if ($('.card-slider').length > 0) {
+  if ($('.card-slider-wrapper').length > 0) {
+    
     // Mobile width corresponds to SM breakpoint from Bootstrap.
     const isMobile = $(window).width() < 768;
 
-    Imagesloaded('.card-slider', () => {
-      const flkty = new Flickity('.card-slider', {
-        cellAlign: 'left',
-        setGallerySize: true,
-        pageDots: isMobile,
-        prevNextButtons: false,
-        contain: true,
-        groupCells: false,
-      });
+    $( ".card-slider-wrapper" ).each( ( index, cardSliderWrapper ) => {
 
-      $('.card-slider-cell').css('height', '100%');
+      let flkty;
+      let cardSlider = $(cardSliderWrapper).find('.card-slider')[0];
 
-      $('#card-slider-prev').on('click', () => {
-        flkty.previous();
-      });
+      Imagesloaded(cardSlider, () => {
 
-      $('#card-slider-next').on('click', () => {
-        flkty.next();
-      });
+         flkty = new Flickity(cardSlider, {
+          cellAlign: 'left',
+          setGallerySize: true,
+          pageDots: isMobile,
+          prevNextButtons: false,
+          contain: true,
+          groupCells: false,
+        });
 
-      flkty.on('select', () => {
-        $('.card-slider-btn').removeClass('disabled');
+        $(cardSliderWrapper).find('#card-slider-prev').on('click', () => {
+          flkty.previous();
+        });
 
-        if (flkty.selectedIndex === 0) {
-          $('#card-slider-prev').addClass('disabled');
-        }
+        $(cardSliderWrapper).find('#card-slider-next').on('click', () => {
+          flkty.next();
+        });
 
-        if (
-          flkty.selectedIndex + 1 === flkty.slides.length ||
-          flkty.selectedIndex + 2 === flkty.slides.length
-        ) {
-          $('#card-slider-next').addClass('disabled');
-        }
+        $(cardSliderWrapper).find('.card-slider-cell').css('height', '100%');
+
+        flkty.on('select', () => {
+          $(cardSliderWrapper).find('.card-slider-btn').removeClass('disabled');
+  
+          if (flkty.selectedIndex === 0) {
+            $(cardSliderWrapper).find('#card-slider-prev').addClass('disabled');
+          }
+  
+          if (
+            flkty.selectedIndex + 1 === flkty.slides.length ||
+            flkty.selectedIndex + 2 === flkty.slides.length
+          ) {
+            $(cardSliderWrapper).find('#card-slider-next').addClass('disabled');
+          }
+        });
+
       });
     });
   }
