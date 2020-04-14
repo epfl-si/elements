aaa7eec950341a9d03a298235a7707e336be5cf3
Grégory Charmier <gregory.charmier@gmail.com>
2020-04-14 06:53:00 +0200

    memento multiple sliders by page

--- assets/components/organisms/card-slider/card-slider.js
+++ assets/components/organisms/card-slider/card-slider.js
@@ -29,28 +29,25 @@ export default () => {
         $(cardSliderWrapper).find('#card-slider-next').on('click', () => {
           flkty.next();
         });
+
+        $(cardSliderWrapper).find('.card-slider-cell').css('height', '100%');
+
+        flkty.on('select', () => {
+          $(cardSliderWrapper).find('.card-slider-btn').removeClass('disabled');
+
+          if (flkty.selectedIndex === 0) {
+            $(cardSliderWrapper).find('#card-slider-prev').addClass('disabled');
+          }
+
+          if (
+            flkty.selectedIndex + 1 === flkty.slides.length ||
+            flkty.selectedIndex + 2 === flkty.slides.length
+          ) {
+            $(cardSliderWrapper).find('#card-slider-next').addClass('disabled');
+          }
+        });
+
       });
-
-
-/*
-      $('.card-slider-cell').css('height', '100%');
-
-
-      flkty.on('select', () => {
-        $('.card-slider-btn').removeClass('disabled');
-
-        if (flkty.selectedIndex === 0) {
-          $('#card-slider-prev').addClass('disabled');
-        }
-
-        if (
-          flkty.selectedIndex + 1 === flkty.slides.length ||
-          flkty.selectedIndex + 2 === flkty.slides.length
-        ) {
-          $('#card-slider-next').addClass('disabled');
-        }
-      });
-      */
     });
   }
 };
