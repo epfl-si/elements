1e45281ed1fd147249dc33be39ee1ee327b80e77
Grégory Charmier <gregory.charmier@gmail.com>
2020-04-14 06:43:13 +0200

    memento multiple sliders by page

--- assets/components/organisms/card-slider/card-slider.js
+++ assets/components/organisms/card-slider/card-slider.js
@@ -5,17 +5,14 @@ import Imagesloaded from 'imagesloaded';
 
 export default () => {
   if ($('.card-slider-wrapper').length > 0) {
-    
     // Mobile width corresponds to SM breakpoint from Bootstrap.
     const isMobile = $(window).width() < 768;
 
     $( ".card-slider-wrapper" ).each( ( index, cardSliderWrapper ) => {
-
       let flkty;
       let cardSlider = $(cardSliderWrapper).find('.card-slider')[0];
 
       Imagesloaded(cardSlider, () => {
-
          flkty = new Flickity(cardSlider, {
           cellAlign: 'left',
           setGallerySize: true,
@@ -32,25 +29,28 @@ export default () => {
         $(cardSliderWrapper).find('#card-slider-next').on('click', () => {
           flkty.next();
         });
-
-        $(cardSliderWrapper).find('.card-slider-cell').css('height', '100%');
-
-        flkty.on('select', () => {
-          $(cardSliderWrapper).find('.card-slider-btn').removeClass('disabled');
-  
-          if (flkty.selectedIndex === 0) {
-            $(cardSliderWrapper).find('#card-slider-prev').addClass('disabled');
-          }
-  
-          if (
-            flkty.selectedIndex + 1 === flkty.slides.length ||
-            flkty.selectedIndex + 2 === flkty.slides.length
-          ) {
-            $(cardSliderWrapper).find('#card-slider-next').addClass('disabled');
-          }
-        });
-
       });
+
+
+/*
+      $('.card-slider-cell').css('height', '100%');
+
+
+      flkty.on('select', () => {
+        $('.card-slider-btn').removeClass('disabled');
+
+        if (flkty.selectedIndex === 0) {
+          $('#card-slider-prev').addClass('disabled');
+        }
+
+        if (
+          flkty.selectedIndex + 1 === flkty.slides.length ||
+          flkty.selectedIndex + 2 === flkty.slides.length
+        ) {
+          $('#card-slider-next').addClass('disabled');
+        }
+      });
+      */
     });
   }
 };
