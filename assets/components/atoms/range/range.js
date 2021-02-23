eb076c80f1203bcbfee33332a1a9d0a1989912c0
Julien Delasoie <julien.delasoie@epfl.ch>
2021-02-23 14:54:27 +0100

    Fix eslint warnings

--- assets/components/atoms/range/range.js
+++ assets/components/atoms/range/range.js
@@ -1,15 +1,13 @@
-/* global $ */
-
 export default () => {
   // Create colored progress bar for damn webkit browsers >.<
-  
+
   const rangeSlider = document.querySelector('input[type=range]');
-  
-  rangeSlider.style.setProperty('--val', + rangeSlider.value);
-  rangeSlider.style.setProperty('--max', + rangeSlider.max);
-  rangeSlider.style.setProperty('--min', + rangeSlider.min);
-  
-  rangeSlider.addEventListener('input', e => {
-    rangeSlider.style.setProperty('--val', + rangeSlider.value);
+
+  rangeSlider.style.setProperty('--val', +rangeSlider.value);
+  rangeSlider.style.setProperty('--max', +rangeSlider.max);
+  rangeSlider.style.setProperty('--min', +rangeSlider.min);
+
+  rangeSlider.addEventListener('input', () => {
+    rangeSlider.style.setProperty('--val', +rangeSlider.value);
   }, false);
 };
