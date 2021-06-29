f99be7a330c6aca35376de355bcc0e3418a5ca1f
Dominique Quatravaux <dominique.quatravaux@epfl.ch>
2021-06-29 20:39:19 +0200

    [WIP] Monkeying with Tom Select API

--- assets/components/atoms/tag/tag-input.js
+++ assets/components/atoms/tag/tag-input.js
@@ -3,12 +3,12 @@
 export default () => {
   new TomSelect('.tag-input', {
     plugins: ['remove_button'],
-    render: {
-      item: (data, escape) => `<div class="tag tag-primary">${escape(data.text)}</div>`,
-    },
-    create: input => ({
-      value: input,
-      text: input,
-    }),
+//    render: {
+//      item: (data, escape) => `<div class="tag tag-primary">${escape(data.text)}</div>`,
+//    },
+//    create: input => ({
+//      value: input,
+//      text: input,
+//    }),
   });
 };
