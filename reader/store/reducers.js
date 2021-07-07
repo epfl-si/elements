cda4696cf3b192b07198e1986602b1020bd3b5bc
Dominique Quatravaux <dominique.quatravaux@epfl.ch>
2021-07-07 22:16:22 +0200

    [remove] yellow bar widget on top that warns of new toolbox releases
    Aaand its assorted Redux whatever.

--- reader/store/reducers.js
+++ reader/store/reducers.js
@@ -1,13 +1,11 @@
 import { combineReducers } from 'redux';
 import { connectRouter } from 'connected-react-router'
 
-import { reducers as alert } from './alert/index';
 import { reducers as atomic } from './atomic/index';
 import { reducers as docs } from './docs/index';
 import { reducers as navigation } from './navigation/index';
 
 const rootReducer = (history) => combineReducers({
-  alert,
   atomic,
   docs,
   navigation,
