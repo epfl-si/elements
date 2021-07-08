e514b6ae9a24c04a39fb40179c8b22ab588a82ea
Dominique Quatravaux <dominique.quatravaux@epfl.ch>
2021-07-08 11:23:23 +0200

    [refactor]&[fix] SVG spritemap preload logic (svg-icons.svg)
    - DRY and halve the size
    - Can totally run after the DOM is loaded, thus side-stepping race conditions whence document.body doesn't exist yet
    - Ensure that all `<div>`s are hidden — We can no longer assume that the toolbox took care of that at build time
    - As a belt to go with these suspenders, put the `<div>` at the end of the `<body>` rather than at the beginning
    - Use DOM API consistently and conservatively (https://developer.mozilla.org/en-US/docs/Web/API/Element/insertAdjacentElement#browser_compatibility says .insertAdjacentElement is OK; caniuse.com concurs, assuming you updated your Firefox since 2016)

--- assets/icons/svg-icons.js
+++ assets/icons/svg-icons.js
@@ -1,28 +1,20 @@
 const svgIcons = () => {
+  loadIcons(window.svgPath || 'icons/icons.svg');
+  loadIcons(window.featherSvgPath || 'icons/feather-sprite.svg');
+};
+
+export default svgIcons;
+
+function loadIcons (svgPath) {
   const ajax = new XMLHttpRequest();
-  const svgPath = window.svgPath || 'icons/icons.svg';
   ajax.open('GET', svgPath, true);
   ajax.send();
   ajax.onload = function (e) {
     if (e.target.status === 200) {
       const div = document.createElement('div');
+      div.setAttribute('style', 'display: none');
       div.innerHTML = ajax.responseText;
-      document.body.insertBefore(div, document.body.childNodes[0]);
+      document.body.insertAdjacentElement('beforeend', div);
     }
   };
-
-  const ajaxFeather = new XMLHttpRequest();
-  const featherSvgPath = window.featherSvgPath || 'icons/feather-sprite.svg';
-  ajaxFeather.open('GET', featherSvgPath, true);
-  ajaxFeather.send();
-  ajaxFeather.onload = function (e) {
-    if (e.target.status === 200) {
-      const div = document.createElement('div');
-      div.innerHTML = ajaxFeather.responseText;
-      div.style = 'display: none;';
-      document.body.insertBefore(div, document.body.childNodes[0]);
-    }
-  };
-};
-
-export default svgIcons;
+}
