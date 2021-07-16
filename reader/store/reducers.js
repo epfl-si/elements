d8671e5642ca205edc50d0ea0a9e008cb2095f86
Dominique Quatravaux <dominique.quatravaux@epfl.ch>
2021-07-16 21:13:32 +0200

    [delete] As much redux (and other cruft) as we can
    - No more `window.sources`, `window.sourcesOrder` — Now available at compile-time from `reader/asset-components.js`
    - Keep the `({ navigation })` Redux channel; delete all the others
    - Delete `window.data` — Not sure when this was last useful, and what for
    
    squash! [delete] As much redux (and other cruft) as we can

--- reader/store/reducers.js
+++ reader/store/reducers.js
@@ -1,15 +1,9 @@
-import { combineReducers } from 'redux';
-import { connectRouter } from 'connected-react-router'
+import { combineReducers } from 'redux'
 
-import { reducers as atomic } from './atomic/index';
-import { reducers as docs } from './docs/index';
-import { reducers as navigation } from './navigation/index';
+import { reducers as navigation } from './navigation/index'
 
-const rootReducer = (history) => combineReducers({
-  atomic,
-  docs,
-  navigation,
-  router: connectRouter(history),
-});
-
-export default rootReducer;
+export default function rootReduce () {
+  return combineReducers({
+    navigation
+  })
+}
