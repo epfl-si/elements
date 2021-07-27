2074d42350fb21c1721a8051aca3433b59082e8a
Dominique Quatravaux <dominique.quatravaux@epfl.ch>
2021-07-27 09:28:40 +0200

    [delete] Even more Redux

--- reader/components/Sidebar/Sidebar.jsx
+++ reader/components/Sidebar/Sidebar.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
 import { NavLink } from 'react-router-dom'
 
--- reader/components/SidebarDocs/SidebarDocs.jsx
+++ reader/components/SidebarDocs/SidebarDocs.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react'
-import { connect } from 'react-redux'
 import { useLocation } from "react-router-dom"
 import Collapse from 'react-css-collapse'
 import PropTypes from 'prop-types'
