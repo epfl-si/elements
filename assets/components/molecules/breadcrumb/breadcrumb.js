c0b27787ade58db1179f2bf4bfa9891dd430d2b9
Aline <aline.keller@gmail.com>
2024-05-16 17:51:21 +0200

    Improve dropdown position on small devices
    The problem: when the breadcrumb item is too close to the right side of the window, the dropdown might be partially hidden outside of the viewport.
    
    The solution: if the width of the dropdown is greater than the space between the button and the right side of the viewport, add class "open-left" to ".dropdown-menu" and use it to change the position of the dropdown.

--- assets/components/molecules/breadcrumb/breadcrumb.js
+++ assets/components/molecules/breadcrumb/breadcrumb.js
@@ -2,8 +2,26 @@
 
 export default () => {
   const expandBreadcrumb = $('.btn-expand-links');
+  const breadcrumbDropToggle = $('.dropdown-toggle');
 
+  // add class 'has-expanded-links'
   expandBreadcrumb.click(function(){
     $(".breadcrumb-wrapper .breadcrumb").addClass("has-expanded-links");
+  });
+
+  // improve dropdown position
+  breadcrumbDropToggle.click(function(){
+    var btnPos = $(this).offset().left;
+    var documentWitdh = $(document).width();
+    var dropdown = $(this).siblings(".dropdown-menu");
+    var dropdownWidth = dropdown.width();
+    var btnOffset = documentWitdh - btnPos;
+
+    // remove class 'open-left' from all .dropdown-menu elements
+    $(".dropdown-menu").removeClass("open-left");
+    // add the class back if the dropdown is too close to the right side of the window
+    if ( dropdownWidth > btnOffset ) {
+      dropdown.addClass("open-left");
+    }
   });
 };
